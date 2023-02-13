4e77f2c684f1850e9bbb6cd6b1db8a86e00e8c55
Jeyhun Mammadsaidov <mcmemmedd@gmail.com>
2023-02-13 04:32:58 +0400

    initial commit

---
+++ src/main/java/az/cmammad/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package az.cmammad.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/cmammad/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package az.cmammad.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
